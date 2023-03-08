22caad7cba09b0c5323e67459ce149a10610b1ec
quantumjot <code@arlowe.co.uk>
2023-03-08 16:09:51 -0500

    add tracks and ids

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,18 +11,33 @@ NODES = {
 
 
 EDGES = {
+    "id": [0, 1, 2, 3, 4, 5, 6, 7],
     "source": [0, 1, 1, 2, 3, 4, 5, 6],
     "target": [1, 2, 3, 4, 5, 6, 6, 7],
+}
+
+TRACKS = {
+    "id": [0, 1, 2, 3],
+    "begin": [0, 2, 3, 6],
+    "end": [1, 4, 5, 7,]
 }
 
 
 @pytest.fixture
 def test_nodes() -> dict[str, list]:
+    """Return the nodes of a simple test graph."""
     return NODES
 
 
 @pytest.fixture 
 def test_edges() -> dict[str, list]:
+    """Return the edges of a simple test graph."""
     return EDGES
+
+
+@pytest.fixture 
+def test_tracks() -> dict[str, list]:
+    """Return the tracks defined in the graph."""
+    return TRACKS
 
 
