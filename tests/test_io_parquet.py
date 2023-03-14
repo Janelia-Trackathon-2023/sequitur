aa56d16df4e8d641d0d1277e5b3b6838ecc5dbdb
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2023-03-14 14:33:24 -0400

    Add io for nodes and edges

--- tests/test_io_parquet.py
+++ tests/test_io_parquet.py
@@ -56,45 +56,45 @@ def test_read_dataframe(my_path, minimum_example):
     assert read_dataframe.equals(dataframe)
 
 
-def test_io_nodemodel(tmpdir, simple_nodes):
+def test_io_nodemodel(tmpdir, example_nodes):
     # write nodes
     path = Path(tmpdir, 'nodes.parquet')
-    _write_parquet_api(path, simple_nodes)
+    _write_parquet_api(path, example_nodes)
 
     assert path.exists()
 
     # read the file back
     nodes_read = read_nodes(path)
 
-    assert nodes_read == simple_nodes
+    assert nodes_read == example_nodes
 
 
-def test_io_edgemodel(tmpdir, simple_edges):
+def test_io_edgemodel(tmpdir, example_edges):
     # write edges
     path = Path(tmpdir, 'edges.parquet')
-    _write_parquet_api(path, simple_edges)
+    _write_parquet_api(path, example_edges)
 
     assert path.exists()
 
     # read the file back
     edges_read = read_edges(path)
 
-    assert edges_read == simple_edges
+    assert edges_read == example_edges
 
 
-def test_io_graph(tmpdir, simple_nodes, simple_edges):
+def test_io_graph(tmpdir, example_nodes, example_edges):
     # write nodes
     node_path = Path(tmpdir, 'nodes.parquet')
-    _write_parquet_api(node_path, simple_nodes)
+    _write_parquet_api(node_path, example_nodes)
 
     # write edges
     edge_path = Path(tmpdir, 'edges.parquet')
-    _write_parquet_api(edge_path, simple_edges)
+    _write_parquet_api(edge_path, example_edges)
 
     # read the file back
     graph = read_graph(node_path, edge_path)
 
-    assert graph['nodes'] == simple_nodes
-    assert graph['edges'] == simple_edges
+    assert graph['nodes'] == example_nodes
+    assert graph['edges'] == example_edges
 
 
