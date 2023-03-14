158b0b8610c88459e84718a2f15df1105d732a55
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2023-03-13 21:34:34 -0400

    Add nodes and edges io

--- sequitur/parquet/parquet_io.py
+++ sequitur/parquet/parquet_io.py
@@ -7,10 +7,15 @@ import pyarrow as pa
 import pyarrow.parquet as pq
 
 from sequitur.format import FileNames
+from sequitur.schema import NodeModel, EdgeModel
 
-ACCEPTED_DATATYPES = dict[str, Any] | DataFrame
+ACCEPTED_DTYPES = list[dict[str, Any]] | dict[str, Any] | DataFrame
 
-def write_parquet(path: Union[str, Path], nodes: ACCEPTED_DATATYPES, edges: ACCEPTED_DATATYPES) -> None:
+def write_parquet(
+        path: Union[str, Path], 
+        nodes: Union[ACCEPTED_DTYPES, list[NodeModel]], 
+        edges: Union[ACCEPTED_DTYPES, list[EdgeModel]] = None
+    ) -> None:
     """Write out a graph."""
 
     # TODO validate path
@@ -18,18 +23,59 @@ def write_parquet(path: Union[str, Path], nodes: ACCEPTED_DATATYPES, edges: ACCE
     if not path.exists():
         path.mkdir(parents=True)
 
+    # TODO check that node and edges are compatible types
+
     # TODO: validate the incoming data
-    if isinstance(nodes, dict):
-        nodes = DataFrame(nodes)
+    if isinstance(nodes, (dict, DataFrame)):
+        nodes_df = DataFrame(nodes)
+
+        # validate nodes
+        required_node_fields = NodeModel.required_fields()
+        missing_fields = set(required_node_fields) - set(nodes.columns)
+        if missing_fields:
+            raise ValueError(
+                f'Missing required node fields: {missing_fields}.'
+            )
+        
+        # write out the nodes to file    
+        _write_parquet_df(path / FileNames.NODES.value, nodes)
+        
+        if edges is not None:
+            if isinstance(edges, dict):
+                edges_df = DataFrame(edges)
+            else:
+                raise ValueError(
+                    'Nodes and edges should be of the same type '
+                    f'(here {type(nodes)} and {type(edges)}).'
+                )
+
+            required_edge_fields = EdgeModel.required_fields()
+            missing_fields = set(required_edge_fields) - set(edges.columns)
+            if missing_fields:
+                raise ValueError(
+                    f'Missing required edges fields: {missing_fields}.'
+                )
+
+
+    _write_parquet_df(path / FileNames.NODES.value, nodes)
+    _write_parquet_df(path / FileNames.EDGES.value, edges)
+
+
+def _write_parquet_api(path: Union[str, Path], data: list):
+    fields = data[0].__fields__.keys()
+
+    table = {}
+    for f in fields:
+        table.update(
+            {
+                f: [getattr(item, f) for item in data]
+            }
+        )
+
+    _write_parquet_df(path, DataFrame.from_dict(table))
     
-    if isinstance(edges, dict):
-        edges = DataFrame(edges)
 
-    _write_parquet(path / FileNames.NODES.value, nodes)
-    _write_parquet(path / FileNames.EDGES.value, edges)
-
-
-def _write_parquet(path: Union[str, Path], dataframe: DataFrame):
+def _write_parquet_df(path: Union[str, Path], dataframe: DataFrame):
     table = pa.Table.from_pandas(dataframe)
     pq.write_table(table, path)
 
--- tests/test_io_parquet.py
+++ tests/test_io_parquet.py
@@ -4,8 +4,11 @@ import pytest
 import numpy as np
 import pandas as pd
 
-from sequitur.parquet.parquet_io import _write_parquet, write_parquet, read_parquet
+from sequitur.parquet.parquet_io import (
+    _write_parquet_df, write_parquet, read_parquet, _write_parquet_api
+)
 from sequitur.format import NodeEntries
+from sequitur.schema import NodeModel, EdgeModel
 
 @pytest.fixture
 def my_path(tmpdir):
@@ -25,7 +28,7 @@ def test_write_dataframe(my_path, minimum_example):
     dataframe = pd.DataFrame(minimum_example)
 
     # write file 
-    _write_parquet(my_path, dataframe)
+    _write_parquet_df(my_path, dataframe)
 
     # check that it exists
     assert Path(my_path).exists()
@@ -36,7 +39,7 @@ def test_read_dataframe(my_path, minimum_example):
     dataframe = pd.DataFrame(minimum_example)
 
     # write file 
-    _write_parquet(my_path, dataframe)
+    _write_parquet_df(my_path, dataframe)
 
     # load
     read_dataframe = read_parquet(my_path)
@@ -59,3 +62,26 @@ def test_read_write_nodes_edges(tmpdir, simple_nodes, simple_edges):
     read_edges = read_parquet(Path(tmpdir, 'edges.parquet'))
     assert read_edges.to_pandas().equals(pd.DataFrame(simple_edges))
 
+def test_io_api(tmpdir):
+    nodes = [
+        NodeModel(node_id=i, coordinates=(i,)) for i in range(5)
+    ]
+
+    path = Path(tmpdir, 'nodes.parquet')
+    _write_parquet_api(path, nodes)
+
+    assert path.exists()
+
+    read_nodes = read_parquet(path)
+
+    # read 
+    fields = nodes[0].__fields__.keys()
+
+    table = {}
+    for f in fields:
+        table.update(
+            {
+                f: [getattr(node, f) for node in nodes]
+            }
+        )
+    assert read_nodes.to_pandas().equals(pd.DataFrame(table))
